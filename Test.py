8571ad66862b7608c595f7b849c0fb007d7165b9
Timothy Goodman <goodmant@cardiff.ac.uk>
2019-03-02 17:56:26 +0000

    made some changes

---
+++ Test.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import messagebox
+import csv
+
+class test_file(Frame):
+	""" Common base class for all test types """
+
+	def __init__ (self, master, testName, module, teacherName):
+		Frame.__init__(self, master)
+		open(testName+'.csv', mode='w')
+		with open("tests_overview.csv", mode = 'a') as csvfile:
+			csvfile.write('{},{},{}\n'.format(module, testName, teacherName))
+		
+
+

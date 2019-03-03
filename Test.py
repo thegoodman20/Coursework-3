2cbfc25bd9203974f8298257ccbbcc9be3ece487
Timothy Goodman <goodmant@cardiff.ac.uk>
2019-03-03 14:12:55 +0000

    This class allows a user to create a test file and append the metadat… …

--- Test.py
+++ Test.py
@@ -1,13 +1,12 @@
-from question import Question
-from tkinter import *
-from tkinter import messagebox
+#from tkinter import *
+#from tkinter import messagebox
 import csv
 
-class test(Frame):
+class test_file:
 	""" Common base class for all test types """
 
-	def __init__ (self, master, testName, module, teacherName):
-		Frame.__init__(self, master)
+	def __init__ (self, testName, module, teacherName):
+		#Frame.__init__(self, master)
 		open(testName+'.csv', mode='w')
 		with open("tests_overview.csv", mode = 'a') as csvfile:
 			csvfile.write('{},{},{}\n'.format(module, testName, teacherName))
